5c5e80366d3405bd7329424de50e01e9509101b8
Aryanil Kundu <142530103+Aryanilkundu@users.noreply.github.com>
2026-08-07 03:30:52 +0530

    Time: 5 ms (75.06%) | Memory: 22.6 MB (53.89%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,12 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        ct=defaultdict(int)
-        seen=set()
-        for i in range(len(nums)):
-            if nums[i] not in seen:
-                ct[nums[i]]+=1
-                if ct[nums[i]] > len(nums)//3:
-                    seen.add(nums[i])
-                if len(seen) ==2:
-                    break
-        return list(seen)
+        # ct=defaultdict(int)
+        # seen=set()
+        # for i in range(len(nums)):
+        #     if nums[i] not in seen:
+        #         ct[nums[i]]+=1
+        #         if ct[nums[i]] > len(nums)//3:
+        #             seen.add(nums[i])
+        #         if len(seen) ==2:
+        #             break
+        # return list(seen)
+        count1 = count2 = 0
+        cand1 = cand2 = None
+        for num in nums:
+            if num == cand1:
+                count1 += 1
+            elif num == cand2:
+                count2 += 1
+            elif count1 == 0:
+                cand1, count1 = num, 1
+            elif count2 == 0:
+                cand2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        # Verify counts
+        return [n for n in (cand1, cand2) if nums.count(n) > len(nums) // 3]
