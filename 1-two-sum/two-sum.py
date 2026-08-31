1e8f318f1df72af3bb80e4224199ecc5513cceed
Aryanil Kundu <142530103+Aryanilkundu@users.noreply.github.com>
2026-08-31 12:56:25 +0530

    Time: 10 ms (35.70%) | Memory: 21.3 MB (7.10%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,22 +1,39 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ls = []
-        nums1 = nums.copy()
-        for element in nums:
-            element1 = target - element 
-            if element1 != element:
-                if element1 in nums:
-                    ls.append(nums.index(element))
-                    ls.append(nums.index(element1))
-                    break
-            else:
-                nums1.pop(nums.index(element))
-                if element1 in nums1:
-                    ls.append(nums.index(element))
-                    ls.append(nums.index(element1,nums.index(element)+1))
-                    break
+        # ls = []
+        # nums1 = nums.copy()
+        # for element in nums:
+        #     element1 = target - element 
+        #     if element1 != element:
+        #         if element1 in nums:
+        #             ls.append(nums.index(element))
+        #             ls.append(nums.index(element1))
+        #             break
+        #     else:
+        #         nums1.pop(nums.index(element))
+        #         if element1 in nums1:
+        #             ls.append(nums.index(element))
+        #             ls.append(nums.index(element1,nums.index(element)+1))
+        #             break
 
-        return ls
+        # return ls
+
+        l = 0 
+        r = len(nums)-1
+        ls = []
+        for index, val in enumerate(nums):
+            ls.append([index,val])
+        ls = sorted(ls,key=lambda i: (i[1], i[0]))
+
+        while l<r:
+            if ls[l][1]+ls[r][1] == target:
+                return [ls[l][0],ls[r][0]]
+            elif ls[l][1]+ls[r][1] > target:
+                r -=1
+            else:
+                l+=1
+        return [l,r]
+            
 
         
 
